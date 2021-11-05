df2cc9a27a442e2ea69d55a10da40f6f047e5676
eduardokayke <eduardokaykedasilva@gmail.com>
2021-11-05 06:26:03 -0300

    Introdução a funções

---
+++ FullStack/GeekUniversity/JavaScript_Geek_University/001_JavaScript_Conceitos_Basicos_1/009_Introducao_Funcoes.js
@@ -0,0 +1,34 @@
+// Introdução as funções.
+
+//------------------------------------------------------
+// function somar(num1, num2) {
+//     var res = num1 + num2;
+
+//     return res;
+// }
+//------------------------------------------------------
+// function somar(num1, num2) {
+//     return num1 + num2;
+// }
+
+// var resultado = somar(6, 4);
+// console.log(resultado);
+//------------------------------------------------------
+
+function somar(num1, num2) {
+    console.log(num1 + num2);
+};
+ 
+function mensagem() {
+    console.log(`Olá a todos.`);
+};
+
+function multiplicar(n1, n2) {
+    return n1 * n2
+}
+
+console.log(multiplicar(3, 5))
+
+somar(6, 4);
+mensagem();
+
