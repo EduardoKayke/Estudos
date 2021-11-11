0d816de0e7183f1a283f1d4ac3eb4fb9b6b80bb9
eduardokayke <eduardokaykedasilva@gmail.com>
2021-11-10 22:10:28 -0300

    Template String

--- FullStack/GeekUniversity/JavaScript_Geek_University/002_JavaScript_Conceitos_Basicos_2/007_Template_String.js
+++ FullStack/GeekUniversity/JavaScript_Geek_University/002_JavaScript_Conceitos_Basicos_2/007_Template_String.js
@@ -1,4 +1,6 @@
 const idade = 19;
 const nome = `Geek`;
 
-console.log(`Esse é um exemplo de template onde consigo por o nome e a idade junto com variáveis e constantes - nome: ${nome} e a idade: ${idade}.`);
+console.log(`Esse é um exemplo de template onde consigo por o nome e a idade junto com variáveis e constantes - nome: ${nome} e a idade: ${idade + 4}.`);
+
+// Podemos fazer operações no template string.
