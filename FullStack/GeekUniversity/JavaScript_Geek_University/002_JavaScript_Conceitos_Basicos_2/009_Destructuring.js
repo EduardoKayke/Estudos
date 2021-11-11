0070fa77261865b99baeeb34d6be32bad780cfcd
eduardokayke <eduardokaykedasilva@gmail.com>
2021-11-11 07:53:46 -0300

    Destructuring

--- FullStack/GeekUniversity/JavaScript_Geek_University/002_JavaScript_Conceitos_Basicos_2/009_Destructuring.js
+++ FullStack/GeekUniversity/JavaScript_Geek_University/002_JavaScript_Conceitos_Basicos_2/009_Destructuring.js
@@ -1,5 +1,49 @@
 /**
- * Destructuring
- * 
- * 
+ * Destructuring extrai dados dos containers.
  */
+
+// Destructuring com array.
+
+const numeros = [1, 3, 5, 7];
+
+const [num1, num2, num3, num4] = numeros;
+
+console.log(num1);
+console.log(num2);
+console.log(num3);
+console.log(num4);
+console.log(`----------------------------------`);
+// Podemos descartar determinado valor;
+const [n1, , n3, n4] = numeros;
+
+console.log(num1);
+console.log(num3);
+console.log(num4);
+console.log(n1, n3, n4);
+console.log(`----------------------------------`);
+
+// Podemos colocar valor default que será usado caso não haja no container.
+
+// const numeros = [1, 3, 5, 7];
+const [numero1, numero2, numero3, numero4 = 34, numero5 = 99] = numeros;
+
+console.log(numero1); // Resultado 1
+console.log(numero2); // Resultado 3
+console.log(numero3); // Resultado 5
+console.log(numero4); // Resultado 7
+console.log(numero5); // Resultado 99 Caso não tivesse 99 seria undefined.
+console.log(`----------------------------------`);
+
+// Destructuring com objeto.
+
+const produto = {
+    nome: `Playstation 4`,
+    preco: 1678.44,
+}
+
+const {nome, preco} = produto;
+
+console.log(nome);
+console.log(preco);
+
+console.log(`${nome} custa ${preco}.`);
