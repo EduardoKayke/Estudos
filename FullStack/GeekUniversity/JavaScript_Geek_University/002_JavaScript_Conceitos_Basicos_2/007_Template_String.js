d87e61622e581e3a8400f055e46848f47ec89f2f
eduardokayke <eduardokaykedasilva@gmail.com>
2021-11-10 22:06:07 -0300

    Template String

---
+++ FullStack/GeekUniversity/JavaScript_Geek_University/002_JavaScript_Conceitos_Basicos_2/007_Template_String.js
@@ -0,0 +1,4 @@
+const idade = 19;
+const nome = `Geek`;
+
+console.log(`Esse é um exemplo de template onde consigo por o nome e a idade junto com variáveis e constantes - nome: ${nome} e a idade: ${idade}.`);
