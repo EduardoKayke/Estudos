cbfbb2e4a2fdc8fd60a69b190d0ed6d0fd1f86c7
eduardokayke <eduardokaykedasilva@gmail.com>
2021-11-16 14:43:16 -0300

    Funções Closures

---
+++ FullStack/GeekUniversity/JavaScript_Geek_University/003_Funcoes/008_Funcoes_Closures.js
@@ -0,0 +1,28 @@
+/**
+ * Funções Closures
+ */
+
+let xuxa = `Global`; // Pode ser acessada globalmente no programa.
+
+function imprimir(){
+    console.log(xuxa);
+}
+
+function outra(){
+    let xuxa = `Local`; // Pode ser acessada localmente no bloco/contexto.
+    imprimir();
+    console.log(xuxa); // Chamou a local.
+}
+
+outra(); // Chamou o Global. E também a local.
+
+// As funções costumam chamar as variáveis de dentro. Se não, as de fora.
+// Closure é o escopo criado quando a função é declarada.
+
+//NOVO EXEMPLO
+
+let variavelGlobal = `Variável Global`;
+
+function externa(){
+    let variavelGlobal = `Variável Local`;
+}
