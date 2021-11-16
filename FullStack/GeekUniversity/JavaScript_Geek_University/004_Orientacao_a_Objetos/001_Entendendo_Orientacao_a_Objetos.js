e89d5ed186fe61ac5dfb14a1b61aaee5fd434874
eduardokayke <eduardokaykedasilva@gmail.com>
2021-11-16 18:39:06 -0300

    Orientação a Objetos

---
+++ FullStack/GeekUniversity/JavaScript_Geek_University/004_Orientacao_a_Objetos/001_Entendendo_Orientacao_a_Objetos.js
@@ -0,0 +1,13 @@
+/**
+ * Orientação a Objetos = Paradigma de Programação.
+ * Paradigma de programação é um meio de se classificar as linguagens de 
+ * programação baseado em suas funcionalidades. As linguagens podem ser 
+ * classificadas em vários paradigmas. Um paradigma de programação fornece e 
+ * determina a visão que o programador possui sobre a estruturação e execução 
+ * do programa.
+ * 
+ * Produto = {nome, preço, desconto} - (Classe = Molde)
+ * 
+ * Notebook - R$: 2.300,00 - 15% (Objetos/Instâncias)
+ * Caneta BIC - R$: 2,94 - 5% (Objetos/Instâncias) 
+ */
