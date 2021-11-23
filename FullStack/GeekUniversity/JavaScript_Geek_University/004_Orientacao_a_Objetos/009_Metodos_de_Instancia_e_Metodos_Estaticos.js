aa3b8cf4ae5794c07c8b395824f2f2e7094511fd
eduardokayke <eduardokaykedasilva@gmail.com>
2021-11-23 15:27:42 -0300

    Static

---
+++ FullStack/GeekUniversity/JavaScript_Geek_University/004_Orientacao_a_Objetos/009_Metodos_de_Instancia_e_Metodos_Estaticos.js
@@ -0,0 +1,34 @@
+class Carro {
+    constructor(modelo){
+        this._marca = Carro.retornarMarca;
+        this._modelo = modelo; 
+    }
+
+    get marca(){
+        return this._marca;
+    }
+
+    get modelo(){
+        return this._modelo;
+    }
+
+    set modelo(modelo){
+        this._modelo = modelo;
+    }
+
+    imprimirDados(){
+        console.log(`${this.marca} ${this.modelo}`);
+    }
+
+    static retornarMarca(){
+        return `Honda`;
+    }
+}
+
+const fit = new Carro(`Fit`);
+
+// fit.imprimirDados();
+
+// console.log(fit.marca);
+
+console.log(Carro.retornarMarca());
