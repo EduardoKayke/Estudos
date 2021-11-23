16e479f8ae8c7cab87a5a519b17ae3f639ca63bf
eduardokayke <eduardokaykedasilva@gmail.com>
2021-11-23 19:18:06 -0300

    DOM

---
+++ FullStack/GeekUniversity/JavaScript_Geek_University/005_Manipulando_a_DOM/001_O_Que_e_a_DOM.js
@@ -0,0 +1,11 @@
+/**
+ * Quando criamos um Documento HTML, toda a estrutura é chamada de DOM.
+ * 
+ * Document Object Model.
+ * 
+ * JavaScript e a API DOM
+ * 
+ * Uma das grandes funcionalidades da linguagem JavaScript é o uso para 
+ * manipulação da DOM. Usamos JavaScript para percorrer e manipular os 
+ * elementos da DOM.
+*/
