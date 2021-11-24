90c9a8e8cf47f0ee11a6bff9452c3369194df51a
eduardokayke <eduardokaykedasilva@gmail.com>
2021-11-24 17:37:51 -0300

    DOM

--- FullStack/GeekUniversity/JavaScript_Geek_University/005_Manipulando_a_DOM/003_HTML_JS_Aula/JS/script.js
+++ FullStack/GeekUniversity/JavaScript_Geek_University/005_Manipulando_a_DOM/003_HTML_JS_Aula/JS/script.js
@@ -27,3 +27,38 @@ console.log(elements[1].innerHTML);
 //Se eu quiser só o primeiro elemento.
 // let elements = document.getElementsByClassName('texto')[0];
 // console.log(elements.innerHTML);
+
+//Podemos mudar tudo o que quisermos.
+elements[0].style.textTransform = 'uppercase';
+// deixamos em letra maiúscula. JAVASCRIPT
+
+//Da pra utilizar o querySelector no lugar dos outros.
+// Podemos fazer todas as buscas de elementos atráves do querySelector.
+let inp = document.querySelector('body div.container div.row input');
+// Pode ser mais simples, pode ser só a tag, só o ID, só a classe.
+let inp1 = document.querySelector('body'); // tag
+let inp2 = document.querySelector('#div1'); // ID
+let inp3 = document.querySelector('.row'); // class
+// Se quisermos todos os elementos usamos querySelectorAll
+let inp4 = document.querySelectorAll('.texto'); // class
+// Outra forma de buscar um elemento.
+let inp5 = document.querySelector('input[name=produto]');
+console.log(inp);
+console.log(inp1);
+console.log(inp2);
+console.log(inp3);
+console.log(inp4);
+console.log(inp5);
+
+//Exemplo 1 de como criar os eventos do html diretamente pelo javascript.
+// let btn = document.querySelector('.btn');
+// btn.onclick = function(){
+    // alert('Eu estou aqui!');
+// }
+
+//Exemplo 2 de como criar os eventos do html diretamente pelo javascript.
+let btn = document.querySelector('.btn');
+let inp6 = document.querySelector('input[name=produto]');
+btn.onclick = function(){
+    alert(`Temos o texto ${inp6.value}`);
+}
