6a7729cf0e0ad7680a67f351f19fd7204d23b617
eduardokayke <eduardokaykedasilva@gmail.com>
2021-11-29 19:47:36 -0300

    Removendo pastas

---
+++ FullStack/GeekUniversity/JavaScript_Geek_University/006_JavaScript_Assincrono/004_Axios.js
@@ -0,0 +1,10 @@
+/**
+ * A ferramenta, a biblioteca mais utilizada atualmente. Tanto pra web,
+ * quanto pra mobile.
+ * 
+ * Essa é uma bibliotca, um cliente HTTP baseado em Promises. 
+ * Navegador e Node.js = Javascript.
+*/
+
+let btn = document.querySelector('#btn');
+let div = document.querySelector('#app');
