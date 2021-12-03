93b86e1aeaafe84aeaf7489df82f3517cf2bb1ae
EduardoKayke <eduardokaykedasilva@gmail.com>
2021-12-03 18:27:42 -0300

    Novas pastas para guardar conteúdo no futuro

--- FullStack/GeekUniversity/JavaScript_Geek_University/006_JavaScript_Assincrono/004_Axios.js
+++ FullStack/GeekUniversity/JavaScript_Geek_University/006_JavaScript_Assincrono/004_Axios.js
@@ -8,3 +8,19 @@
 
 let btn = document.querySelector('#btn');
 let div = document.querySelector('#app');
+
+btn.onclick = function(){
+    //Limpar a div.
+    div.innerHTML = '';
+
+    //Criar o span.
+    let spanNome = document.createElement('span');
+
+    //Criar o txtNome.
+    let txtNome = '';
+
+    //Recuperar o input.
+    let github_user = document.querySelector('input[name=github_user').Value;
+
+    //
+}
