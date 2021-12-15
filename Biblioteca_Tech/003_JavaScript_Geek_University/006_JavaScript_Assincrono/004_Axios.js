e787328cd106205c390096101eb8d4a797143e31
EduardoKayke <eduardokaykedasilva@gmail.com>
2021-12-15 09:55:43 -0300

    Axios

--- Biblioteca_Tech/003_JavaScript_Geek_University/006_JavaScript_Assincrono/004_Axios.js
+++ Biblioteca_Tech/003_JavaScript_Geek_University/006_JavaScript_Assincrono/004_Axios.js
@@ -20,7 +20,38 @@ btn.onclick = function(){
     let txtNome = '';
 
     //Recuperar o input.
-    let github_user = document.querySelector('input[name=github_user').Value;
+    let github_user = document.querySelector('input[name=github_user');
 
-    //
+    //Limpando o input
+    let user = github_user.value;
+    github_user.value = '';
+
+    //GET, POST, PUT, DELETE (Poderiam ser)
+    axios.get(`https://api.github.com/users/${user}`)
+        // Se sucesso execute
+        .then(function(response){
+            if(response.data.name !== null){
+                txtNome = document.createTextNode(response.data.name);
+
+                let img = document.createElement('img');
+                img.setAttribute('src', response.data.avatar_url);
+                img.setAttribute('alt', response.data.name);
+                img.setAttribute('width', '45px');
+                img.setAttribute('height', '45px');
+
+                div.appendChild(img);
+            }else{
+                txtNome = document.createTextNode(`Não encontrado`);
+            }
+            //adiciona o conteúdo na div
+            spanNome.appendChild(txtNome);
+            div.appendChild(spanNome);            
+        })
+        // Caso falhe execute
+        .catch(function(error){
+            txtNome = document.createTextNode('Falha na requisição.')
+            //adiciona o conteúdo na div
+            spanNome.appendChild(txtNome);
+            div.appendChild(spanNome);
+        });
 }
