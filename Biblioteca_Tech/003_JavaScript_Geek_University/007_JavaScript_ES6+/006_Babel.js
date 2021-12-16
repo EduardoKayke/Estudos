78bee78446df272db503391b505cfeddc2473838
EduardoKayke <eduardokaykedasilva@gmail.com>
2021-12-16 13:06:31 -0300

    Babel

---
+++ Biblioteca_Tech/003_JavaScript_Geek_University/007_JavaScript_ES6+/006_Babel.js
@@ -0,0 +1,21 @@
+/**Babel
+ * É uma biblioteca.
+ * O yarn é um gerenciador de pacotes pra JavaScript e Node.
+ * 
+ * Babel = Reescreve o seu código para que qualquer avegador
+ * entenda.
+ * 
+ * Comando no terminal
+ * 
+ * yarn init (Iniciar o yarn)
+ * 
+ * yarn add @babel/cli -D (Trazer a biblioteca do babel)
+ * 
+ * yarn add @babel/preset-env -D (Pega o código javascript e 
+ * prepara o código idependende do local onde está sendo  
+ * desenvolvido)
+ * 
+ * yarn add @babel/core -D (x)
+ * 
+ * yarn add babel-loader -D (x)
+*/
