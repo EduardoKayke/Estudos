5a888a4370dcfba026f00cc0aa8dd511ce8da6cd
EduardoKayke <eduardokaykedasilva@gmail.com>
2021-12-16 16:16:27 -0300

    Babel

--- Biblioteca_Tech/003_JavaScript_Geek_University/007_JavaScript_ES6+/006_Babel.js
+++ Biblioteca_Tech/003_JavaScript_Geek_University/007_JavaScript_ES6+/006_Babel.js
@@ -15,7 +15,35 @@
  * prepara o código idependende do local onde está sendo  
  * desenvolvido)
  * 
- * yarn add @babel/core -D (x)
- * 
- * yarn add babel-loader -D (x)
+ * yarn add @babel/core -D
+ * yarn add babel-loader -D 
 */
+
+// Para usar o babel, criar um arquivo (.babelrc)  
+// Escrever dentro.
+
+// {
+//     "presets": ["@babel/preset-env"]
+// }
+
+
+// Script de execução. Terminal: comando - (yarn dev)
+// Esse código é um comando do cli, é como pedir pra
+// transpilar o arquivo. 
+// Dessa forma ele será reescrito.
+// Escrever abaixo dos pacotes no arquivo package.json
+// O primeiro é ./NOME DO ARQUIVO segue exemplo abaixo.
+
+// "scripts":{
+//     "dev": "babel ./main.js -o ./bundle.js -w"
+// }
+
+// LEMBRETE para executar é yarn mais o nome do script dado
+// Aqui no caso é dev. (yarn dev) Só abre se estiver na pasta.
+// Consegui rodar com main.js
+// Se colocar -w ele entra automaticamente todas as 
+// alterações do nosso arquivo main.js.
+
+// Depois podemos usar o bundle.js no nosso HTML.
+// dessa forma o babel cuida de todos os arquivos js
+// configurados.
