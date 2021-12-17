8cef30aa9a4d15e43742de5fd1971f672e2526f6
EduardoKayke <eduardokaykedasilva@gmail.com>
2021-12-17 19:03:19 -0300

    Operadores Aritméticos Teoria

---
+++ Biblioteca_Tech/001_Algoritmo_Logica_de_Programacao/002_Operadores/003_Operadores_Aritmeticos_Teoria.js
@@ -0,0 +1,13 @@
+// Operadores Aritméticos Teoria.
+
+// soma ( + ), subtração ( - ), multiplicação ( * ), divisão ( / ).
+
+// Também temos o operador módulo.
+
+// Módulo ( % ) = Resto da divisão. Sempre dará 1 ou 0.
+// 1 ou 0 para ímpar e par restantes de divisões.
+let resto = 10 % 3;
+console.log(resto); // 1
+
+let restoPar = 10323243438 % 2;
+console.log(restoPar); // 0
