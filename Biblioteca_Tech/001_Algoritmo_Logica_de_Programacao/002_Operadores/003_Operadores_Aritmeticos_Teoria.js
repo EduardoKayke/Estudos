aaa2082764d252c1e5dff30ee74f17442d795446
EduardoKayke <eduardokaykedasilva@gmail.com>
2021-12-17 19:23:45 -0300

    Operadores Aritméticos Prática

--- Biblioteca_Tech/001_Algoritmo_Logica_de_Programacao/002_Operadores/003_Operadores_Aritmeticos_Teoria.js
+++ Biblioteca_Tech/001_Algoritmo_Logica_de_Programacao/002_Operadores/003_Operadores_Aritmeticos_Teoria.js
@@ -4,8 +4,7 @@
 
 // Também temos o operador módulo.
 
-// Módulo ( % ) = Resto da divisão. Sempre dará 1 ou 0.
-// 1 ou 0 para ímpar e par restantes de divisões.
+// Módulo ( % ) = Resto da divisão. 
 let resto = 10 % 3;
 console.log(resto); // 1
 
---
+++ Biblioteca_Tech/001_Algoritmo_Logica_de_Programacao/002_Operadores/004_Operadores_Aritmeticos_Pratica.js
@@ -0,0 +1,22 @@
+// Operadores Aritméticos Prática
+
+// Valores literais.
+
+console.log(7 + 3); // 10
+console.log(6 - 7); // -1
+console.log(7 * 9); // 63
+console.log(8 / 3); // 2,66666
+console.log(8 % 3); // 2
+
+let a = 8;
+let b = 11;
+
+// Usando variáveis.
+console.log(a + b); // 19
+console.log(a - b); // -3
+console.log(a * b); // 88
+console.log(a / b); // 0.727272
+console.log(a % b); // 8
+
+a = a + b * a / b;
+console.log(a); // 16
