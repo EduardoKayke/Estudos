829bdcbe4095175ff28c70e56f54892dd9ab5b9d
EduardoKayke <eduardokaykedasilva@gmail.com>
2021-12-17 18:23:45 -0300

    Dados

---
+++ Biblioteca_Tech/002_Curso_Web/001_Iniciando_Com_Programacao/004_Dados.js
@@ -0,0 +1,8 @@
+// PI * raio * raio
+
+const PI = Math.PI
+let raio = 10;
+
+let areaCirc = PI * raio * raio;
+
+console.log(`O valor da área é ${areaCirc.toFixed(2)}m².`);
---
+++ Biblioteca_Tech/002_Curso_Web/001_Iniciando_Com_Programacao/005_Dados.js
@@ -0,0 +1,5 @@
+let a = 7;
+let b = 94;
+
+[a, b] = [94, 7];
+console.log(a, b);
