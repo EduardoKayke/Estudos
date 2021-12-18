ede5f6a80f85e90e168e2cbdcd73aa879aefec98
EduardoKayke <eduardokaykedasilva@gmail.com>
2021-12-17 21:36:57 -0300

    Operadores Relacionais teoria

---
+++ Biblioteca_Tech/001_Algoritmo_Logica_de_Programacao/002_Operadores/006_Operadores_Relacionais_Teoria.js
@@ -0,0 +1,11 @@
+// Operadores Relacionais Teoria.
+
+//  >   -  maior que.
+//  >=  -  maior ou igual.
+//  <   -  menor que.
+//  <=  -  menor ou igual.
+//  ==  -  igual a.
+//  !=  -  diferente de.
+
+//  A todos eles o resultado será ou verdairo (true),
+// ou será falso (false).
