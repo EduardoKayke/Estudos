de72122db27cc927494180d66fa71ee05f398071
EduardoKayke <eduardokaykedasilva@gmail.com>
2021-12-20 18:49:54 -0300

    Desafio Operadores Relacionais

---
+++ Biblioteca_Tech/001_Algoritmo_Logica_de_Programacao/002_Operadores/008_Desafio_Operadores_Relacionais.js
@@ -0,0 +1,32 @@
+let a = 1;
+let b = 2;
+
+// Encontre 5 expressões que o resutado é true.
+console.log(a < b);
+console.log(a != b);
+console.log(b > a);
+console.log(b != a);
+console.log(a !== b);
+
+// Encontre 5 expressões que o resutado é false.
+console.log(a > b);
+console.log(a == b);
+console.log(b < a);
+console.log(b == a);
+console.log(a === b);
+
+
+// Exemplo do professor;
+// Encontre 5 expressões que o resutado é true.
+console.log(a < b);
+console.log(a <= b);
+console.log(a != b);
+console.log(a !== b);
+console.log(a + a === b);
+
+// Encontre 5 expressões que o resutado é false.
+console.log(a > b);
+console.log(a >= b);
+console.log(a == b);
+console.log(a === b);
+console.log(a + a !== b);
