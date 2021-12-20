69e85c5eb4d4dbf6ad5045395cf36084c8a79103
EduardoKayke <eduardokaykedasilva@gmail.com>
2021-12-20 18:39:04 -0300

    Operadores Relacionais Prática

--- Biblioteca_Tech/001_Algoritmo_Logica_de_Programacao/002_Operadores/006_Operadores_Relacionais_Teoria.js
+++ Biblioteca_Tech/001_Algoritmo_Logica_de_Programacao/002_Operadores/006_Operadores_Relacionais_Teoria.js
@@ -5,7 +5,9 @@
 //  <   -  menor que.
 //  <=  -  menor ou igual.
 //  ==  -  igual a.
+//  === -  significa estritamente igual.
 //  !=  -  diferente de.
+//  !== -  significa estritamente diferentes
 
 //  A todos eles o resultado será ou verdairo (true),
 // ou será falso (false).
--- Biblioteca_Tech/001_Algoritmo_Logica_de_Programacao/002_Operadores/007_Operadores_Relacionais_Pratica.js
+++ Biblioteca_Tech/001_Algoritmo_Logica_de_Programacao/002_Operadores/007_Operadores_Relacionais_Pratica.js
@@ -16,3 +16,16 @@ console.log(a < b);
 console.log(a <= b);
 console.log(a == b);
 console.log(a != b);
+
+
+let c = 1;
+let d = "1";
+let e = "1";
+console.log(c == d); // True, o conteúdo.
+console.log(c === d); // False, não é 100% igual.
+console.log(d === e); // True, é 100% igual.
+// === significa estritamente igual
+// !== significa estritamente diferentes
+// O exemplo acima vale para todos os sinais. 
+console.log(c != d); // False, o conteúdo é igual.
+console.log(c !== d); // True, pq não é 100% igual.
