36241765c944450be7a96ebb02d5514133286575
EduardoKayke <eduardokaykedasilva@gmail.com>
2021-12-20 20:18:14 -0300

    Operadores de Atribuição Prática.

---
+++ Biblioteca_Tech/001_Algoritmo_Logica_de_Programacao/002_Operadores/009_Operadores_de_Pratica.js
@@ -0,0 +1,27 @@
+// Atribuição é o símbolo de igual.
+
+// =
+// +=
+// -=
+// *=
+// /=
+// %=
+
+let x = 10;
+x = x + 6;
+x = x - 1;
+
+let y = 20;
+y += 10; // Atribuição aditiva.
+y -= 2; // Atribuição subtrativa.
+y *= 2; // Atribuição multiplicativa.
+y /= 4; // Atribuição divisiva.
+y %= 2; // Atribuição módulo.
+
+console.log(x);
+console.log(y);
+
+let w = 'Texto'; 
+w += ', mais texto.';
+
+console.log(w);
