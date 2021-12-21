10f3ca7c579d51570f6f1cf2db84601950b33044
EduardoKayke <eduardokaykedasilva@gmail.com>
2021-12-21 16:00:06 -0300

    Desafio Operadores de Atribuição

---
+++ Biblioteca_Tech/001_Algoritmo_Logica_de_Programacao/002_Operadores/010_Desafio_Operadores_de_Atribuicao.js
@@ -0,0 +1,23 @@
+
+/** Lista de aprovados
+ *  ==================
+ *  
+ *  1. Ana Silva
+ *  2. Pedro Albuquerque
+ *  3. Guilherme Pereira
+ *  4. Rebeca França
+ */
+
+
+// let texto = 'Bom dia!';
+// texto += '\nTudo bem?';
+// console.log(texto);
+
+let texto = 'Lista de aprovados\n';
+texto += '==================\n';
+texto += '1. Ana Silva\n';
+texto += '2. Pedro Albuquerque\n';
+texto += '3. Guilherme Pereira\n';
+texto += '4. Rebeca França\n';
+
+console.log(texto);
