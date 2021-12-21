d4fcfe1d49a4c235fd0f778d26f78e911987d0d9
EduardoKayke <eduardokaykedasilva@gmail.com>
2021-12-21 17:30:25 -0300

    Operadores Lógicos Teoria

---
+++ Biblioteca_Tech/001_Algoritmo_Logica_de_Programacao/002_Operadores/011_Operadores_Logicos_Teoria.js
@@ -0,0 +1,57 @@
+/**
+ * Operadores Lógicos 
+ * 
+ * Operador and = &&
+ * 
+ * true and true = true
+ * true and false = false
+ * false and true = false
+ * false and false = false
+ * 
+*/
+
+/**
+ * Operadores Lógicos 
+ * 
+ * Operador xor = !=
+ * 
+ * true != true = false (LEMBRETE)
+ * true != false = true
+ * false != true = true
+ * false != false = false (LEMBRETE)
+ * 
+*/
+
+/**
+ * Operadores Lógicos 
+ * 
+ * Operador or = ||
+ * 
+ * true || true = true
+ * true || false = true
+ * false || true = true
+ * false || false = false
+ * 
+*/
+
+/**
+ * Operadores Lógicos 
+ * 
+ * Operador or = ||
+ * 
+ * true || true = true
+ * true || false = true
+ * false || true = true
+ * false || false = false
+ * 
+*/
+
+/**
+ * Operadores Lógicos 
+ * 
+ * Operador not = !
+ * 
+ * not true = false
+ * not false = true
+ * 
+*/
