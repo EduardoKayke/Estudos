4ff91dbd5e65fb34e904e5188b253b48f680590c
EduardoKayke <eduardokaykedasilva@gmail.com>
2021-12-21 19:26:23 -0300

    Desafio Operadores Logicos

---
+++ Biblioteca_Tech/001_Algoritmo_Logica_de_Programacao/002_Operadores/013_desafio_Operadores_logicos.js
@@ -0,0 +1,14 @@
+// Duas expressões resultar em TRUE (Aritméticos/Relacionais/Lógicos)
+let true1 = 2 + 5 >= 7 && 8 > 2;
+let true2 = !(6 * 6 < 3);
+
+console.log(true1);
+console.log(true2);
+
+
+// Duas expressões resultar em FALSE (Aritméticos/Relacionai/Lógicos)
+let false1 = 8 - 4 > 12 || false || 15 === '15';
+let false2 = !(7 !== 7 || 3 !== '3');
+
+console.log(false1);
+console.log(false2);
