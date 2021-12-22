f8679f9cbb19241f8ab990efc4205ce06b9a81c4
EduardoKayke <eduardokaykedasilva@gmail.com>
2021-12-21 22:31:53 -0300

    Operadores Unários

--- Biblioteca_Tech/001_Algoritmo_Logica_de_Programacao/002_Operadores/013_desafio_Operadores_logicos.js
+++ Biblioteca_Tech/001_Algoritmo_Logica_de_Programacao/002_Operadores/013_desafio_Operadores_logicos.js
@@ -8,7 +8,7 @@ console.log(true2);
 
 // Duas expressões resultar em FALSE (Aritméticos/Relacionai/Lógicos)
 let false1 = 8 - 4 > 12 || false || 15 === '15';
-let false2 = !(7 !== 7 || 3 !== '3');
+let false2 = !(7 !== 6 + 1 || 3 !== '3');
 
 console.log(false1);
 console.log(false2);
---
+++ Biblioteca_Tech/001_Algoritmo_Logica_de_Programacao/002_Operadores/014_Operadores_Unarios.js
@@ -0,0 +1,20 @@
+/**
+ * Operadores unários
+ * 
+ * ! = Negação Lógica é um operador unário.
+ * 
+ *  a + b = infix, operador no meio dos operandos.
+ *  a++ = postfix, operador após os operandos.
+ *  ++a = prefix. operador antes dos operandos.
+*/
+
+let a = 1;
+
+a = a + 1;
+a += 1;
+a++; // ++ Operador Unário. 
+++a;
+a--;
+--a;
+
+console.log(a); // a = 4
