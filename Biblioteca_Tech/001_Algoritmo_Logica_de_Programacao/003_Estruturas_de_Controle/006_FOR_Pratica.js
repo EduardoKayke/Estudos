34ecc16f2672e4d338ec3c3c43d0d4f4038883e5
EduardoKayke <eduardokaykedasilva@gmail.com>
2021-12-23 18:03:58 -0300

    FOR Prática

---
+++ Biblioteca_Tech/001_Algoritmo_Logica_de_Programacao/003_Estruturas_de_Controle/006_FOR_Pratica.js
@@ -0,0 +1,21 @@
+// Declaração, expressão e o incremento.
+// Para somar de dois em dois controle += 2 (controle++)
+for (let controle = 1; controle <= 10; controle++) {
+    console.log(controle);
+}
+
+console.log("Fim.");
+
+// formas d criar um laço for infinito
+
+// for(;;) {
+// }
+
+// for (; true; ) {
+// }
+
+// Desafio
+
+for (let i = 7; i >= 0; i--) {
+    console.log(i);
+}
