dbc294ff640370f92c69b22c7c0ea81d3403d1ed
EduardoKayke <eduardokaykedasilva@gmail.com>
2021-12-23 11:01:41 -0300

    If/Else Prática

--- Biblioteca_Tech/001_Algoritmo_Logica_de_Programacao/003_Estruturas_de_Controle/003_IF_e_ELSE_Pratica.js
+++ Biblioteca_Tech/001_Algoritmo_Logica_de_Programacao/003_Estruturas_de_Controle/003_IF_e_ELSE_Pratica.js
@@ -10,3 +10,26 @@ if (false)
     console.log('Não exec...');
 else
     console.log('Exec...');
+
+// Outro exemplo.
+
+const hora = 5;
+let saudacao;
+
+if (hora < 12 && hora >= 6) {
+    saudacao = 'Bom dia!';
+} else if (hora < 18 && hora >= 12) {
+        saudacao = 'Boa tarde!';
+} else if (hora <= 24 && hora >= 18){
+    saudacao = 'Boa noite!';
+} else if (hora >= 0 && hora < 6) {
+    saudacao = 'Madrugada... Vai dormir!';
+} else {
+    console.log('Esse valor não existe!');
+}
+
+if (hora <= 24 && hora >= 0) {
+    console.log(saudacao);
+} else {
+    console.log('Por favor, corrija os valores!');
+}
