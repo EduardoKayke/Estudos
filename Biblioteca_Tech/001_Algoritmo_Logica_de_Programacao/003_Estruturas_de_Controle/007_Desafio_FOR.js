bb51fbaf5813d9245ee44e3a14861b0c0761bb2f
EduardoKayke <eduardokaykedasilva@gmail.com>
2021-12-27 13:58:33 -0300

    Desafio FOR

---
+++ Biblioteca_Tech/001_Algoritmo_Logica_de_Programacao/003_Estruturas_de_Controle/007_Desafio_FOR.js
@@ -0,0 +1,18 @@
+// #
+// ##
+// ###
+// ####
+// #####
+// ######
+
+let linha = '';
+
+for(let i = 1; i <= 6; i++){
+    linha += '#';
+    console.log(linha);
+}
+
+// Desafio é fazer  mesma coisa mas sem valor númerico.
+for(let s = '#'; s != '#######'; s += '#'){
+    console.log(s);
+}
