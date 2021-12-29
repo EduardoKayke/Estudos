39f158551993a3b80245784408e907b5bc0ccc00
EduardoKayke <eduardokaykedasilva@gmail.com>
2021-12-29 08:03:34 -0300

    Desafio Função

---
+++ Biblioteca_Tech/001_Algoritmo_Logica_de_Programacao/004_Funcoes/003_Desafio_Funcao.js
@@ -0,0 +1,15 @@
+// Função: 2 params -> console.log(+)
+
+function somar(num1, num2){
+    console.log(num1 + num2);
+}
+
+somar(2, 3);
+
+// Função: 2 params -> console.log(-)
+
+function subtrair(num1, num2){
+    console.log(num1 - num2);
+}
+
+subtrair(2, 3);
