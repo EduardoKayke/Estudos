6e8aa51d30f34b5a68a4a09ad5d549b259a6f7ef
EduardoKayke <eduardokaykedasilva@gmail.com>
2022-01-06 11:49:21 -0300

    Armazenando Funções em Variáveis

--- Biblioteca_Tech/001_Algoritmo_Logica_de_Programacao/004_Funcoes/008_Armazenando_Funcoes_Em_Variaveis.js
+++ Biblioteca_Tech/001_Algoritmo_Logica_de_Programacao/004_Funcoes/008_Armazenando_Funcoes_Em_Variaveis.js
@@ -6,3 +6,14 @@ const somar = function (a, b) {
 }
 
 console.log(somar(2, 4));
+
+// Mais sobre o assunto.
+
+function subtrair(a, b){
+    return a - b;
+}
+
+const subtracao = subtrair;
+
+console.log(subtrair(5, 2));
+console.log(subtracao(5, 2));
