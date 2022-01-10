4fc14718000c6d4ecbece7ee09c4379141174962
EduardoKayke <eduardokaykedasilva@gmail.com>
2022-01-10 13:20:52 -0300

    Basico sobre Objeto

---
+++ Biblioteca_Tech/001_Algoritmo_Logica_de_Programacao/001_Basico_sobre_Objetos_1.js
@@ -0,0 +1,15 @@
+const caneta = {
+    nome: 'Caneta',
+    preco: 9.99,
+    desconto: 0.2
+}; // Object
+
+console.log(typeof(caneta)); 
+
+console.log(caneta['nome']); 
+console.log(caneta['preco']); 
+console.log(caneta['desconto']); 
+
+console.log(caneta.nome); // Forma mais usada pra acessar.
+console.log(caneta.preco); // Forma mais usada pra acessar.
+console.log(caneta.desconto); // Forma mais usada pra acessar.
