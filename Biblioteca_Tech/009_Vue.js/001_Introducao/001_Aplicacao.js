c5e1400039485e2e08517b95f11c8580099a09ad
EduardoKayke <eduardokaykedasilva@gmail.com>
2022-02-21 20:56:40 -0300

    Vue v2

---
+++ Biblioteca_Tech/009_Vue.js/001_Introducao/001_Aplicacao.js
@@ -0,0 +1,11 @@
+new Vue({
+    el: '#app',
+    data: {
+        titulo: "Usando VueJS 2!"
+    },
+    methods: {
+        alterarTitulo(event) {
+            this.titulo = event.target.value
+        },
+    },
+});
