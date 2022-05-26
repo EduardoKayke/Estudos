a2ccb75c0ced8a6a87a75a5cee0902494c94ef81
EduardoKayke <hakuzosu.developer@gmail.com>
2022-05-26 17:39:46 -0300

    Desafio Operadores Aritméticos

---
+++ Biblioteca_Tech/011_Python_3_Cod3r/001_Fundamentos_Python/006_Desafio_Operadores_Aritmeticos.py
@@ -0,0 +1,8 @@
+#Qual é o percentual das despesas em relação ao salário.
+
+salario = 3450.45
+despesas = 2456.2
+
+percentual_comprometido = despesas / salario * 100
+
+print(percentual_comprometido)
