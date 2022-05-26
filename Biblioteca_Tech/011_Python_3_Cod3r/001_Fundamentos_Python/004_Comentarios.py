c0d48904e25ebbd9a386c61e2f1e68dbfdffe75f
EduardoKayke <hakuzosu.developer@gmail.com>
2022-05-26 02:00:54 -0300

    Comentários

---
+++ Biblioteca_Tech/011_Python_3_Cod3r/001_Fundamentos_Python/004_Comentarios.py
@@ -0,0 +1,14 @@
+#Comentários
+
+#Minhas variáveis
+salario = 3450.45
+despesas = 2456.2
+
+"""
+A ideia é calcular o quanto vai sobrar
+no final do mês
+"""
+print(salario - despesas)
+
+# print('Fim!')
+print('Fim de verdade!') #Comentário aqui também vale!
