a50fd4fd12fe342f3b7a7a85993d2e4ecc235319
EduardoKayke <hakuzosu.developer@gmail.com>
2022-05-26 01:49:48 -0300

    Variáveis

---
+++ Biblioteca_Tech/011_Python_3_Cod3r/001_Fundamentos_Python/003_Variaveis.py
@@ -0,0 +1,11 @@
+# Variáveis
+
+a = 10
+b = 5.2
+print(a + b)
+
+a = 'Agora sou uma string'
+print(a)
+
+#print(a + b) Isso leva a um erro, pois agora uma váriavel é string e a outra float.
+#Para resolver o problema devemos colocar o identificador
