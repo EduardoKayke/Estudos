85c44aeb2552dc6a8595f86a0d6326cee2602249
EduardoKayke <hakuzosu.developer@gmail.com>
2022-05-26 01:36:55 -0300

    Tipos Básicos

---
+++ Biblioteca_Tech/011_Python_3_Cod3r/001_Fundamentos_Python/002_Tipos_Basicos.py
@@ -0,0 +1,21 @@
+# Tipos Básicos
+
+print(True) #Boolean
+print(False) #Boolean
+print(1.2 + 1) #Float
+print('Aqui eu falo na minha lingua!') #String aspas simples
+print('Tabém funciona!') #String aspas duplas
+
+print('Você é ' + 3 * 'muito ' + 'legal!')
+# Você é muito muito muito legal!
+
+# print(3 + '3')
+# ERROR unsupported operand type(s) for +: 'int' and 'str'
+
+lista = [1, 2, 3]
+print(lista) #Lista
+
+dicionario = {'nome': 'Pedro', 'idade': 22}
+print(dicionario) #Dicionário chave/valor
+
+print(None) #Ausência de valor, tipo o null
