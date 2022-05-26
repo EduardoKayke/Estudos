18669585fca35b8bde2795d8ba84467bfc030e8b
EduardoKayke <hakuzosu.developer@gmail.com>
2022-05-26 02:14:31 -0300

    Operadores Aritméticos

---
+++ Biblioteca_Tech/011_Python_3_Cod3r/001_Fundamentos_Python/005_Operadores_Aritmeticos.py
@@ -0,0 +1,15 @@
+# Operadores Aritméticos
+#Operadores Aritméticos são conhecidos como operadores binários. (Dupla de operandos)
+
+print(2 + 3) # 5
+print(4 - 7) # -3
+print(2 * 5.3) # 10.6
+print(9.4 / 3) # 3.133333333
+print(9.4 // 3) # 3.0
+print(2 ** 8) # 256
+print(10 % 3) # 1
+
+a = 12
+b = a
+
+print(a + b) # 24
