a91a2a00d4e8dacb2eab01fcc1c1e56e485ed042
EduardoKayke <hakuzosu.developer@gmail.com>
2022-05-27 01:27:35 -0300

    Numbers Alguns Cuidados

--- Desenvolvedor_Full_Stack/003_Curso_Web/001_Iniciando_Com_Programacao/004_Dados.js
+++
@@ -1,8 +0,0 @@
-// PI * raio * raio
-
-const PI = Math.PI
-let raio = 10;
-
-let areaCirc = PI * raio * raio;
-
-console.log(`O valor da área é ${areaCirc.toFixed(2)}m².`);
---
+++ Desenvolvedor_Full_Stack/003_JavaScript/001_JavaScript_Fundamentos/002_Numbers_Alguns_Cuidados.js
@@ -0,0 +1,8 @@
+console.log(7 / 0); // Infinity
+console.log(10 / '2'); // 5
+console.log('Show!' * 2); // NaN
+console.log(0.1 + 0.7); // 0.799999999999999
+
+// console.log(10.toString()); Não funciona a conversão a versão abaixo funciona!
+console.log((10.345).toString());
+console.log((10.345).toFixed(2));
