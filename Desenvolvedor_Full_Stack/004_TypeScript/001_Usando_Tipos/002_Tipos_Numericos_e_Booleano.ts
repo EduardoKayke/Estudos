6ead894442623c31e9e367f4d2634733c937e360
EduardoKayke <hakuzosu.developer@gmail.com>
2022-05-28 16:22:02 -0300

    Tipos Numéricos e Booleano

--- Desenvolvedor_Full_Stack/004_TypeScript/001_Usando_Tipos/002_Tipos_Numericos_e_Booleano.ts
+++ Desenvolvedor_Full_Stack/004_TypeScript/001_Usando_Tipos/002_Tipos_Numericos_e_Booleano.ts
@@ -1,2 +1,12 @@
 // Numbers
 let idade = 27
+// idade = 'Ana' 
+// Não podemos mudar, pois já foi atribuido um tipo númerico pra variável
+idade = 27.5
+console.log(idade);
+
+//Booleans
+let possuiHobbies = false
+// possuiHobbies = 1
+// Não podemos mudar, pois já foi atribuido um tipo booleano pra variável
+console.log(possuiHobbies);
