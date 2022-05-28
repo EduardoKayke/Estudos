106fcbac122ff9f91de59983251ebf676c9ae69c
EduardoKayke <hakuzosu.developer@gmail.com>
2022-05-28 18:26:14 -0300

    Tuplas

---
+++ Desenvolvedor_Full_Stack/004_TypeScript/001_Usando_Tipos/005_Tuplas.ts
@@ -0,0 +1,9 @@
+// Tuplas é uma quantidade pré definida de tipos.
+// Exemplo, quero um array de uma string e dois numbers.
+
+// Tuplas, tem que ser na ordem, caso contrário da erro.
+let endereco: [string, number, string] = ['Av Principal', 99, '']
+console.log(endereco)
+
+endereco = ['Rua Importante', 1260, 'Bloco A']
+console.log(endereco)
