ee77e077be5288b6da6cb4412ac6358158fd455d
EduardoKayke <hakuzosu.developer@gmail.com>
2022-05-31 21:21:26 -0300

    Operadores Atribuição

---
+++ Desenvolvedor_Full_Stack/003_JavaScript/001_JavaScript_Fundamentos/024_Operadores_Atribuicao.js
@@ -0,0 +1,17 @@
+const a = 7
+let b = 3
+
+b += a // igual a (b = b + a)
+console.log(b); // 10
+
+b -= 4 // igual a (b = b - 4)
+console.log(b); // 6
+
+b *= 2 // igual a (b = b * 2)
+console.log(b); // 12
+
+b /= 2 // igual a (b = b / 2)
+console.log(b); // 6
+
+b %= 2 // igual a (b = b % 2)
+console.log(b); // 0
