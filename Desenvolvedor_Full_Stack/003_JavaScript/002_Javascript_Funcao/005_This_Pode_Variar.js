735699533a63bb971f41f23d6dd8c5c1bf1c2dc1
EduardoKayke <hakuzosu.developer@gmail.com>
2022-06-05 18:33:14 -0300

    This Pode Variar

---
+++ Desenvolvedor_Full_Stack/003_JavaScript/002_Javascript_Funcao/005_This_Pode_Variar.js
@@ -0,0 +1,9 @@
+// Assistir a aula 92 do curso web moderno. Caso ainda não saiba.
+
+//Quando você tem uma função tradicional em JavaScript, definida com "function", o this pode variar.
+// Dependendo de como chama a função o this pode ser o window ou um objeto um elemento, tudo vai 
+//depender de como está sendo acessado essa função.
+
+// Mas com funções arrows o this não varia nunca, sendo assim o this vai ser definido quando a função
+//foi definida. Pegando o contexto no qual a função foi definida.
+// Contexto léxico. Ou seja, se a função for escrita no contexto global, aponta pra global.
