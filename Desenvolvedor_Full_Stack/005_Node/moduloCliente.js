acfae0fa8827e09f71ae9d2b7668ed87cdae83fe
Eduardo Kayke <hakuzosu.developer@gmail.com>
2022-07-06 21:04:51 -0300

    Módulos Node

--- Desenvolvedor_Full_Stack/005_Node/moduloCliente.js
+++ Desenvolvedor_Full_Stack/005_Node/moduloCliente.js
@@ -1,3 +1,26 @@
 // Vamos importar
 const moduloA = require('./moduloA')
 const moduloB = require('./moduloB')
+
+console.log(moduloA.ola); 
+// Fala Pessoa
+
+console.log(moduloA.bemVindo);
+// Bem Vindo ao Node!
+
+console.log(moduloA.ateLogo);
+// Até o próximo exemplo!
+
+console.log(moduloA);
+//{ ola: 'Fala Pessoal',
+//  bemVindo: 'Bem Vindo ao Node!',
+//  ateLogo: 'Até o próximo exemplo!' }
+
+console.log(moduloB.bomDia);
+// Bom dia
+
+console.log(moduloB.boaNoite());
+// Boa noite
+
+console.log(moduloB);
+//{ bomDia: 'Bom dia', boaNoite: [Function: boaNoite] }
