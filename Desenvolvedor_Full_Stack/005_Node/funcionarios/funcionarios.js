b92720725b81e39162798b9d11f9646ed5b7cbad
Eduardo Kayke <hakuzosu.developer@gmail.com>
2022-07-07 21:25:13 -0300

    JSON

--- Desenvolvedor_Full_Stack/005_Node/funcionarios/funcionarios.js
+++ Desenvolvedor_Full_Stack/005_Node/funcionarios/funcionarios.js
@@ -5,8 +5,31 @@ const url = 'http://files.cod3r.com.br/curso-js/funcionarios.json'
 // importar o axios
 const axios = require('axios')
 
+const chineses = funcionario => funcionario.pais === 'China'
+const mulheres = funcionario => funcionario.genero === 'F'
+const menorSalario = (funcionario, funcionarioAtual) => {
+    return funcionario.salario < funcionarioAtual.salario ? funcionario : funcionarioAtual
+} 
+
 axios.get(url).then(response => {
     // dentro do data dessa resposta temos a lista de funcionarios
     const funcionarios = response.data
-    console.log(funcionarios);
+    // console.log(funcionarios);
+
+    // Achar a mulher chinesa com menor salário
+    const funcionario = funcionarios
+        .filter(chineses)
+        .filter(mulheres)
+        .reduce(menorSalario)
+    
+    console.log(funcionario);
+    //{ id: 443,
+    //    nome: 'Shela',
+    //    sobrenome: 'Nowell',
+    //    email: 'snowellca@upenn.edu',
+    //    genero: 'F',
+    //    cidade: 'Yufa',
+    //    pais: 'China',
+    //    empresa: 'Quinu',
+    //    salario: 2435.61 }
 })
